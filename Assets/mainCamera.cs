dd5899eacd95cf2da44840e1ad34565211d8d179
Vijay Sharma <vijay.sharm@gmail.com>
2020-02-06 10:54:19 -0500

    Added TestFairy

---
+++ Assets/mainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TestFairyUnity;
+
+public class mainCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestFairy.begin("5b3af35e59a1e074e2d50675b1b629306cf0cfbd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
