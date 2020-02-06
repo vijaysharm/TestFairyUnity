d7320eac0266bd94a0ab0cb2fd91e0abb257dbdc
Vijay Sharma <vijaysharm@users.noreply.github.com>
2020-02-06 13:59:48 -0500

    Update MyBuildPostprocessor.cs

--- Assets/MyBuildPostprocessor.cs
+++ Assets/MyBuildPostprocessor.cs
@@ -1,44 +1,30 @@
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.Callbacks;
 using System.Collections;
-using UnityEditor.iOS.Xcode;
 using System.IO;
 
+#if UNITY_IOS
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+#endif
+
 public class MyBuildPostprocessor {
-	internal static void CopyAndReplaceDirectory(string srcPath, string dstPath)
-	{
-		if (Directory.Exists(dstPath))
-			Directory.Delete(dstPath);
-		if (File.Exists(dstPath))
-			File.Delete(dstPath);
+  [PostProcessBuildAttribute]
+  public static void OnPostprocessBuild(BuildTarget buildTarget, string path) {
+#if UNITY_IOS
+    Debug.Log("OnPostprocessBuild: Path [{" + path + "}]");
+    if (buildTarget == BuildTarget.iOS) {
+      string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
 
-		Directory.CreateDirectory(dstPath);
+      PBXProject proj = new PBXProject();
+      proj.ReadFromString(File.ReadAllText(projPath));
+      var mainTarget = proj.GetUnityMainTargetGuid();
 
-		foreach (var file in Directory.GetFiles(srcPath))
-			File.Copy(file, Path.Combine(dstPath, Path.GetFileName(file)));
+      Debug.Log("OnPostprocessBuild: Adding strip script to target: [" + mainTarget + "]");
+      proj.AddShellScriptBuildPhase(mainTarget, "Strip unused architectures", "/bin/sh", "Assets/Plugins/iOS/TestFairy.framework/strip-architectures.sh");
 
-		foreach (var dir in Directory.GetDirectories(srcPath))
-			CopyAndReplaceDirectory(dir, Path.Combine(dstPath, Path.GetFileName(dir)));
-	}
-
-	[PostProcessBuild]
-	public static void OnPostprocessBuild(BuildTarget buildTarget, string path) {
-		Debug.Log("OnPostprocessBuild: Path [{" + path + "}]");
-		if (buildTarget == BuildTarget.iOS) {
-			string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-
-			PBXProject proj = new PBXProject();
-			proj.ReadFromString(File.ReadAllText(projPath));
-
-// 			var mainTarget = proj.TargetGuidByName("Unity-iPhone");
-// 			var mainTarget = proj.TargetGuidByName();
-			var mainTarget = proj.GetUnityMainTargetGuid();
-
-			Debug.Log("OnPostprocessBuild: Adding strip script to target: [" + mainTarget + "]");
-			proj.AddShellScriptBuildPhase(mainTarget, "Strip unused architectures", "/bin/sh", "Assets/Plugins/iOS/TestFairy.framework/strip-architectures.sh");
-
-			File.WriteAllText(projPath, proj.WriteToString());
-		}
-	}
+      File.WriteAllText(projPath, proj.WriteToString());
+    }
+  }
+#endif
 }
