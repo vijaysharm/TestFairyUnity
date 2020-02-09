f63527fc5404b84036bbcf12b0f5e5e7635d48bd
Vijay Sharma <vijay.sharm@gmail.com>
2020-02-09 06:49:11 -0500

    Move Build file to editor directory

---
+++ Assets/Editor/MyBuildPostprocessor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class MyBuildPostprocessor {
+  [PostProcessBuildAttribute(1)]
+  public static void OnPostprocessBuild(BuildTarget buildTarget, string path) {
+    Debug.Log("OnPostprocessBuild: Path [{" + path + "}]");
+    if (buildTarget == BuildTarget.iOS) {
+      string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+      PBXProject proj = new PBXProject();
+      proj.ReadFromString(File.ReadAllText(projPath));
+      var mainTarget = proj.GetUnityMainTargetGuid();
+
+      Debug.Log("OnPostprocessBuild: Adding strip script to target: [" + mainTarget + "]");
+      proj.AddShellScriptBuildPhase(mainTarget, "Strip unused architectures", "/bin/sh", "Assets/Plugins/iOS/TestFairy.framework/strip-architectures.sh");
+
+      File.WriteAllText(projPath, proj.WriteToString());
+    }
+  }
+}
