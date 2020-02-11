df1771ba2296dfd28aee5459e338a73e6559da48
Vijay Sharma <vijay.sharm@gmail.com>
2020-02-11 09:16:34 -0500

    Added logging

--- Assets/mainCamera.cs
+++ Assets/mainCamera.cs
@@ -8,12 +8,17 @@ public class mainCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+				Application.logMessageReceivedThreaded += CaptureLogThread;
         TestFairy.begin("5b3af35e59a1e074e2d50675b1b629306cf0cfbd");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+			Debug.Log("I love logging");
     }
+
+		void CaptureLogThread(string message, string stacktrace, LogType type) {
+			TestFairy.log(message);
+		}
 }
